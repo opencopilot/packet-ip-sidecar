15a41cfd875e7f6f8377d6ba622ffe5f33181607
Patrick DeVivo <patrick.devivo@gmail.com>
2018-08-30 11:20:41 -0400

    allow lib usage

--- main.go
+++ main.go
@@ -1,210 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net"
 	"os"
 	"os/signal"
-	"strconv"
-	"strings"
 	"syscall"
 	"time"
 
-	"github.com/packethost/packetmetadata/packetmetadata"
-	"github.com/packethost/packngo"
-	"github.com/vishvananda/netlink"
+	"github.com/opencopilot/packet-ip-sidecar/ip"
 )
-
-var (
-	doNotRemoveNets = []string{
-		"fe80::3c50:1dff:fec5:4e5f/64",
-		"169.254.0.0/16",
-	}
-	tag = "unknown"
-)
-
-// AddDummy creates a dummy interface called packet0
-func AddDummy() error {
-	err := netlink.LinkAdd(&netlink.Dummy{
-		netlink.LinkAttrs{
-			Name: "packet0",
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	dummy, err := netlink.LinkByName("packet0")
-	if err != nil {
-		return err
-	}
-
-	err = netlink.LinkSetUp(dummy)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// RemoveDummy removes a dummy interface called packet0
-func RemoveDummy() error {
-	dummy, err := netlink.LinkByName("packet0")
-	if err != nil {
-		return err
-	}
-
-	err = netlink.LinkDel(dummy)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// AddIP parses and adds an IP block to a network interface
-func AddIP(link netlink.Link, addr string) error {
-	a, err := netlink.ParseAddr(addr)
-	if err != nil {
-		return err
-	}
-	err = netlink.AddrReplace(link, a)
-	if err != nil {
-		return err
-	}
-	fmt.Println("adding IP: " + addr)
-	return nil
-}
-
-// RemoveIP parses and remove an IP block from a network interface
-func RemoveIP(link netlink.Link, addr string) error {
-	ip, _, err := net.ParseCIDR(addr)
-	if err != nil {
-		return err
-	}
-
-	a, err := netlink.ParseAddr(addr)
-	if err != nil {
-		return err
-	}
-
-	// Do not remove the IP if it's in doNotRemoveNets
-	for _, subnet := range doNotRemoveNets {
-		_, ipnet, err := net.ParseCIDR(subnet)
-		if err != nil {
-			return err
-		}
-		if ipnet.Contains(ip) {
-			return nil
-		}
-	}
-
-	err = netlink.AddrDel(link, a)
-	if err != nil {
-		return err
-	}
-	fmt.Println("removing IP: " + addr)
-	return nil
-}
-
-// EnsureIPs watches Packet metadata and ensures that any IP blocks added to an instance are added to the packet0 dummy interface
-func EnsureIPs(quit chan bool) {
-	iterator, err := packetmetadata.Watch()
-	if err != nil {
-		log.Println(err)
-	}
-
-	for {
-		select {
-		case <-quit:
-			iterator.Close()
-			break
-		default:
-			res, err := iterator.Next()
-			if err != nil {
-				log.Println(err)
-			}
-
-			packetIF, err := netlink.LinkByName("packet0")
-			if err != nil {
-				log.Println(err)
-			}
-
-			if packetIF == nil {
-				log.Println("packet0 interface does not exist")
-				continue
-			}
-
-			existingAddrs, err := netlink.AddrList(packetIF, netlink.FAMILY_ALL)
-			if err != nil {
-				log.Println(err)
-			}
-
-			incomingAddrs := make([]*packngo.IPAddressAssignment, 0)
-			for _, incomingAddr := range res.Metadata.Instance.Network {
-				if !incomingAddr.Management {
-					incomingAddrs = append(incomingAddrs, incomingAddr)
-				}
-			}
-
-			// iterate over IPs that should be added
-			for _, addr := range incomingAddrs {
-
-				ipBlock := addr.Address + "/" + strconv.Itoa(addr.CIDR)
-				incomingAddr, err := netlink.ParseAddr(ipBlock)
-				if err != nil {
-					log.Println(err)
-				}
-
-				// look for IPs that are not currently added
-				alreadyAdded := false
-				for _, existingAddr := range existingAddrs {
-					if existingAddr.Equal(*incomingAddr) {
-						alreadyAdded = true
-						break
-					}
-				}
-				if !alreadyAdded { // if ip does not exist locally, but should
-					err := AddIP(packetIF, ipBlock)
-					if err != nil {
-						log.Println(err)
-					}
-				}
-			}
-
-			// iterate over IPs that are already added
-			for _, addr := range existingAddrs {
-				shouldKeep := false
-				for _, incomingAddr := range incomingAddrs {
-					ipBlock := incomingAddr.Address + "/" + strconv.Itoa(incomingAddr.CIDR)
-					incomingAddr, err := netlink.ParseAddr(ipBlock)
-					if err != nil {
-						log.Println(err)
-					}
-
-					if addr.Equal(*incomingAddr) {
-						shouldKeep = true
-						break
-					}
-				}
-				if !shouldKeep { // if ip exists locally, but should not
-					err = RemoveIP(packetIF, strings.Split(addr.String(), " ")[0])
-					if err != nil {
-						log.Println(err)
-					}
-				}
-			}
-		}
-	}
-}
 
 func main() {
-	err := AddDummy()
+	err := ip.AddDummy()
 	if err != nil {
 		log.Println(err)
 	}
+	log.Println("added packet0 interface")
 
 	quit := make(chan bool, 1)
-	go EnsureIPs(quit)
+	go ip.EnsureIPs(quit)
 
 	var gracefulStop = make(chan os.Signal)
 	signal.Notify(gracefulStop, syscall.SIGTERM)
@@ -214,7 +28,7 @@ func main() {
 	log.Println("received stop signal, shutting down")
 	quit <- true
 
-	err = RemoveDummy()
+	err = ip.RemoveDummy()
 	if err != nil {
 		log.Println(err)
 	}
